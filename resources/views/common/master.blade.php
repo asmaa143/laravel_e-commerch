defb7046a89582b3f1ca668baf23d90d749d7ac8
AsmaaMohamedKhraiba <Asm2kh@gmail.com>
2021-12-13 20:51:20 +0200

    first_commit

---
+++ resources/views/common/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
+</head>
+<body>
+@yield('content')
+<script src="{{asset('assets/js/jquery-3.4.1.js')}}"></script>
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/all.min.js')}}"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['prefix'=>'login'],function (){
+
+    Route::get('/',[AuthController::class,"getLoginView"])->name('login');
+    Route::post('/',[AuthController::class,"doLogin"]);
+});
+
+Route::group(['prefix'=>'register'],function (){
+
+    Route::get('/',[AuthController::class,"getRegisterView"]);
+    Route::post('/',[AuthController::class,"doRegister"]);
+});
+
+
+
+Route::get('/logout',[AuthController::class,"doLogout"]);
+
+Route::group(['prefix'=>'contact-us'],function (){
+    Route::get('/',[ContactController::class,"getContactView"]);
+    Route::post('/',[ContactController::class,"saveFeedback"]);
+});
+
+Route::group(['prefix'=>'tests'],function (){
+    Route::get('/upload-file',[TestController::class,"getUploadView"]);
+    Route::post('/upload-file',[TestController::class,"doUpload"]);
+
+});
+
+
+//middleware  //ok
+
+// controller(update,delete)
+
+//route naming //ok
+
+//Api , dashboard
+
+
